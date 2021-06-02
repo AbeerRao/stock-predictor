123dac3f3f19a9f852f277cb040d96c7b97f946b
AbeerRao <abeerrao@icloud.com>
2021-06-02 03:50:35 +0000

    K-NN score: 56.86

--- predict.py
+++ predict.py
@@ -1,5 +1,5 @@
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix, accuracy_score
 import numpy as np
@@ -20,7 +20,7 @@ sc = StandardScaler()
 x_train[:, :] = sc.fit_transform(x_train[:, :])
 x_test[:, :] = sc.transform(x_test[:, :])
 
-classifer = LogisticRegression(random_state=0)
+classifer = KNeighborsClassifier(n_neighbors=5)
 classifer.fit(x_train, y_train)
 
 # y_pred = classifer.predict(x_test)
